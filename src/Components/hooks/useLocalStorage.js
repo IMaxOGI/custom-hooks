6b1a2c5cc0b80234bacaea94d292072969dbea8f
IMaxOGI <57328875+IMaxOGI@users.noreply.github.com>
2021-01-16 20:51:05 +0200

    Update

--- src/Components/hooks/useLocalStorage.js
+++ src/Components/hooks/useLocalStorage.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
-export default function useLocalStorage() {
-  const [value, setValue] = useState(localStorage.getItem("name") || "");
+export default function useLocalStorage(name, val) {
+  const [value, setValue] = useState(localStorage.getItem(name) || "");
 
   useEffect(() => {
-    localStorage.setItem("name", value);
-  }, [value]);
+    localStorage.setItem(name, val);
+  }, [name, val]);
 
   return [value, setValue];
 }
